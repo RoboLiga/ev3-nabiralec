fbc36ee28a27c2408835de6e8b01b921443af779
Nejc Ilc <nejc.ilc@gmail.com>
2019-02-04 08:56:48 +0100

    Renamed some variables to be more consistent.

--- nabiralec.py
+++ nabiralec.py
@@ -43,9 +43,9 @@ MOTOR_LEFT_PORT = 'outA'
 MOTOR_RIGHT_PORT = 'outD'
 
 # Najvišja dovoljena hitrost motorjev.
-SPEED_LIMIT = 900
+SPEED_MAX = 900
 # Najvišja dovoljena nazivna hitrost motorjev pri vožnji naravnost.
-# Naj bo manjša kot SPEED_LIMIT, da ima robot še možnost zavijati.
+# Naj bo manjša kot SPEED_MAX, da ima robot še možnost zavijati.
 SPEED_BASE_MAX = 800
 
 # Parametri za PID
@@ -53,12 +53,12 @@ SPEED_BASE_MAX = 800
 PID_TURN_KP = 3.0
 PID_TURN_KI = 0.5
 PID_TURN_KD = 0.0
-PID_TURN_INT_LIMIT = 100
+PID_TURN_INT_MAX = 100
 # Nazivna hitrost pri vožnji naravnost.
 PID_STRAIGHT_KP = 2.0
 PID_STRAIGHT_KI = 0.5
 PID_STRAIGHT_KD = 0.01
-PID_STRAIGHT_INT_LIMIT = 100
+PID_STRAIGHT_INT_MAX = 100
 
 # Dolžina FIFO vrste za hranjenje meritev (oddaljenost in kot do cilja).
 HIST_QUEUE_LENGTH = 3
@@ -69,9 +69,9 @@ DIST_EPS = 20
 # Dovoljena napaka pri obračanju [stopinje].
 DIR_EPS = 5
 # Bližina cilja [mm].
-DIST_CLOSE = 100
+DIST_NEAR = 100
 # Koliko sekund je robot lahko stanju vožnje naravnost v bližini cilja
-# (oddaljen manj kot DIST_CLOSE), preden sprožimo varnostni mehanizem
+# (oddaljen manj kot DIST_NEAR), preden sprožimo varnostni mehanizem
 # in ga damo v stanje obračanja na mestu.
 TIMER_NEAR_TARGET = 3
 
@@ -447,7 +447,7 @@ PID_turn = PID(
     Kp=PID_TURN_KP,
     Ki=PID_TURN_KI,
     Kd=PID_TURN_KD,
-    integral_limit=PID_TURN_INT_LIMIT)
+    integral_limit=PID_TURN_INT_MAX)
 
 # PID za vožnjo naravnost - regulira nazivno hitrost za oba motorja,
 # ki je odvisna od oddaljenosti od cilja.
@@ -457,7 +457,7 @@ PID_frwd_base = PID(
     Kp=PID_STRAIGHT_KP,
     Ki=PID_STRAIGHT_KI,
     Kd=PID_STRAIGHT_KD,
-    integral_limit=PID_STRAIGHT_INT_LIMIT)
+    integral_limit=PID_STRAIGHT_INT_MAX)
 
 # PID za obračanje med vožnjo naravnost.
 # setpoint=0 pomeni, da naj bo kot med robotom in ciljem (target_angle) enak 0.
@@ -466,7 +466,7 @@ PID_frwd_turn = PID(
     Kp=PID_TURN_KP,
     Ki=PID_TURN_KI,
     Kd=PID_TURN_KD,
-    integral_limit=PID_TURN_INT_LIMIT)
+    integral_limit=PID_TURN_INT_MAX)
 
 # Hitrost na obeh motorjih.
 speed_right = 0
@@ -608,7 +608,7 @@ while do_main_loop and not btn.down:
                     timer_near_target = TIMER_NEAR_TARGET
 
                 # Ali smo blizu cilja?
-                robot_near_target = target_dist < DIST_CLOSE
+                robot_near_target = target_dist < DIST_NEAR
                 if not robot_near_target_old and robot_near_target:
                     # Vstopili smo v bližino cilja.
                     # Začnimo odštevati varnostno budilko.
@@ -647,13 +647,13 @@ while do_main_loop and not btn.down:
             # Omejimo vrednosti za hitrosti na motorjih.
             speed_right = round(
                             min(
-                                max(speed_right, -SPEED_LIMIT),
-                                SPEED_LIMIT)
+                                max(speed_right, -SPEED_MAX),
+                                SPEED_MAX)
                             )
             speed_left = round(
                             min(
-                                max(speed_left, -SPEED_LIMIT),
-                                SPEED_LIMIT)
+                                max(speed_left, -SPEED_MAX),
+                                SPEED_MAX)
                             )
 
             # Vrtimo motorje.
